097c2fd9942608301aff4a3c46d3de4f351dee46
marcelgs13 <marcelgstv@gmail.com>
2026-08-13 18:01:16 -0300

    chore: configure Entity Framework Core with SQLite

---
+++ Data/BibliotecaDbContext.cs
@@ -0,0 +1,36 @@
+using BibliotecaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BibliotecaAPI.Data;
+
+public class BibliotecaDbContext(DbContextOptions<BibliotecaDbContext> options) : DbContext(options)
+{
+    public DbSet<Autor> Autores => Set<Autor>();
+    public DbSet<Livro> Livros => Set<Livro>();
+    public DbSet<Aluno> Alunos => Set<Aluno>();
+    public DbSet<Emprestimo> Emprestimos => Set<Emprestimo>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Autor>()
+            .HasMany(autor => autor.Livros)
+            .WithOne(livro => livro.Autor)
+            .HasForeignKey(livro => livro.AutorId);
+
+        modelBuilder.Entity<Aluno>()
+            .HasMany(aluno => aluno.Emprestimos)
+            .WithOne(emprestimo => emprestimo.Aluno)
+            .HasForeignKey(emprestimo => emprestimo.AlunoId);
+
+        modelBuilder.Entity<Livro>()
+            .HasMany(livro => livro.Emprestimos)
+            .WithOne(emprestimo => emprestimo.Livro)
+            .HasForeignKey(emprestimo => emprestimo.LivroId);
+
+        modelBuilder.Entity<Aluno>()
+            .HasIndex(aluno => aluno.Matricula)
+            .IsUnique();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using BibliotecaAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<BibliotecaDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
